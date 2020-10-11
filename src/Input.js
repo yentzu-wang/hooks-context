732f32033b1ebc3791bb732f4beb6dbd4d52f17f
Frank <frank7300228@gmail.com>
2020-10-11 15:41:38 +0800

    Finish 9-109

--- src/Input.js
+++ src/Input.js
@@ -2,7 +2,23 @@ import React from "react"
 import PropTypes from "prop-types"
 
 const Input = () => {
-  return <div data-test="component-input"></div>
+  const [currentGuess, setCurrentGuess] = React.useState("")
+
+  return (
+    <div data-test="component-input">
+      <form clasName="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+        />
+        <button data-test="submit-button" className="btn btn-parimary mb-2">
+          Submit
+        </button>
+      </form>
+    </div>
+  )
 }
 
 Input.propTypes = {
