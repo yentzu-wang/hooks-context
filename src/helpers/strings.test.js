842c3ebf6a78f2505306be9616b702d8921eb58f
Frank <frank7300228@gmail.com>
2020-10-12 20:43:39 +0800

    Finish 11-126

---
+++ src/helpers/strings.test.js
@@ -0,0 +1,11 @@
+import stringModule from "./strings"
+
+const { getStringByLanguage } = stringModule
+
+test("returns correct submit string for english", () => {})
+
+test("returns the correct submit string for emoji", () => {})
+
+test("returns english submit string when language does not exist", () => {})
+
+test("returns english submit string when submit key does not exist for language", () => {})
