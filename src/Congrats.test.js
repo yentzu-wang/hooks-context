406ccb177a6db0b2ff0ae7d9db6556e26188ffb6
Frank <frank7300228@gmail.com>
2020-10-13 15:34:01 +0800

    Finish 11-137

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -24,26 +24,35 @@ const setup = ({ success, language }) => {
 }
 
 describe("languagePicker", () => {
-  test("correctly renders congrats string in english", () => {})
+  test("correctly renders congrats string in english", () => {
+    const wrapper = setup({ success: true })
+    expect(wrapper.text()).toBe("Congratulations! You guessed the word!")
+  })
 
-  test("correctly renders congrats string in emoji", () => {})
+  test("correctly renders congrats string in emoji", () => {
+    const wrapper = setup({ success: true, language: "emoji" })
+    expect(wrapper.text()).toBe("")
+  })
 })
 
 test("renders without error", () => {
-  const wrapper = setup()
+  const wrapper = setup({})
   const component = findByTestAttr(wrapper, "component-congrats")
   expect(component.length).toBe(1)
 })
+
 test("renders no text when `success` prop is false", () => {
   const wrapper = setup({ success: false })
   const component = findByTestAttr(wrapper, "component-congrats")
   expect(component.text()).toBe("")
 })
+
 test("renders non-empty congrats message when `success` prop is true", () => {
   const wrapper = setup({ success: true })
   const message = findByTestAttr(wrapper, "congrats-message")
   expect(message.text().length).not.toBe(0)
 })
+
 test("does not throw warning with expected props", () => {
   const expectedProps = { success: false }
   checkProps(Congrats, expectedProps)
