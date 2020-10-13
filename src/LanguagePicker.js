a15b1a9253c164d92a8b3004ed43543d1fed5b0f
Frank <frank7300228@gmail.com>
2020-10-13 13:59:45 +0800

    Finish 11-131

--- src/LanguagePicker.js
+++ src/LanguagePicker.js
@@ -1,7 +1,12 @@
 import React from "react"
+import PropTypes from "prop-types"
 
 const LanguagePicker = ({ setLanguage }) => {
-  return <div>LanguagePicker</div>
+  return <div data-test="component-language-picker">LanguagePicker</div>
+}
+
+LanguagePicker.propTypes = {
+  setLanguage: PropTypes.func.isRequired
 }
 
 export default LanguagePicker
--- src/LanguagePicker.test.js
+++ src/LanguagePicker.test.js
@@ -1,10 +1,23 @@
 import React from "react"
 import { shallow } from "enzyme"
+import LanguagePicker from "./LanguagePicker"
 import { findByTestAttr, checkProps } from "../test/testUtils"
 
-test("renders without error", () => {})
+const mockSetLanguage = jest.fn()
 
-test("does not throw warning with expected props", () => {})
+const setup = () => {
+  return shallow(<LanguagePicker setLanguage={mockSetLanguage} />)
+}
+
+test("renders without error", () => {
+  const wrapper = setup()
+  const component = findByTestAttr(wrapper, "component-language-picker")
+  expect(component.exists()).toBe(true)
+})
+
+test("does not throw warning with expected props", () => {
+  checkProps(LanguagePicker, { setLanguage: jest.fn() })
+})
 
 test("renders non-zero language icons", () => {})
 
