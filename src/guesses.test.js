0d36d44de97e506cc04437049f02e9fdda2bb774
Frank <frank7300228@gmail.com>
2020-10-15 16:53:03 +0800

    Finish 12-157

--- src/guesses.test.js
+++ src/guesses.test.js
@@ -1,18 +1,29 @@
 import React from "react"
 import { mount } from "enzyme"
 import Input from "./Input"
+import GuessedWords from "./GuessedWords"
 import successContext from "./contexts/successContext"
+import guessedWordsContext from "./contexts/guessedWordsContext"
 import { findByTestAttr } from "../test/testUtils"
 
-function setup(secretWord = "party") {
+function setup(guessedWords = [], secretWord = "party") {
   const wrapper = mount(
-    <successContext.SuccessProvider>
-      <Input secretWord={secretWord} />
-    </successContext.SuccessProvider>
+    <guessedWordsContext.GuessedWordsProvider>
+      <successContext.SuccessProvider>
+        <Input secretWord={secretWord} />
+        <GuessedWords />
+      </successContext.SuccessProvider>
+    </guessedWordsContext.GuessedWordsProvider>
   )
 
   const inputBox = findByTestAttr(wrapper, "input-box")
   const submitButton = findByTestAttr(wrapper, "submit-button")
+
+  guessedWords.forEach(word => {
+    const mockEvent = { target: { value: word } }
+    inputBox.simulate("change", mockEvent)
+    submitButton.simulate("click")
+  })
 
   return [wrapper, inputBox, submitButton]
 }
