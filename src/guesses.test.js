ec10cc5e98e72b674f0573a6e550dfbb82cdf9e4
Frank <frank7300228@gmail.com>
2020-10-15 17:22:55 +0800

    Finish 12-159

--- src/guesses.test.js
+++ src/guesses.test.js
@@ -73,4 +73,19 @@ describe("test word guesses", () => {
       })
     })
   })
+
+  describe("empty guessWords", () => {
+    beforeEach(() => {
+      ;[wrapper, inputBox, submitButton] = setup([], "party")
+    })
+
+    test("guessedWords shows correct guesses after incorrect guess", () => {
+      const mockEvent = { target: { value: "train" } }
+      inputBox.simulate("change", mockEvent)
+      submitButton.simulate("click")
+
+      const guessedWordsTableRows = findByTestAttr(wrapper, "guessed-word")
+      expect(guessedWordsTableRows.length).toBe(1)
+    })
+  })
 })
