fca8b586d31f01eb4047961469a7a9ab4d9e0e6e
Frank <frank7300228@gmail.com>
2020-10-15 17:12:53 +0800

    Finish 12-158

--- src/guesses.test.js
+++ src/guesses.test.js
@@ -6,7 +6,7 @@ import successContext from "./contexts/successContext"
 import guessedWordsContext from "./contexts/guessedWordsContext"
 import { findByTestAttr } from "../test/testUtils"
 
-function setup(guessedWords = [], secretWord = "party") {
+function setup(guessedWordsStrings = [], secretWord = "party") {
   const wrapper = mount(
     <guessedWordsContext.GuessedWordsProvider>
       <successContext.SuccessProvider>
@@ -19,7 +19,7 @@ function setup(guessedWords = [], secretWord = "party") {
   const inputBox = findByTestAttr(wrapper, "input-box")
   const submitButton = findByTestAttr(wrapper, "submit-button")
 
-  guessedWords.forEach(word => {
+  guessedWordsStrings.forEach(word => {
     const mockEvent = { target: { value: word } }
     inputBox.simulate("change", mockEvent)
     submitButton.simulate("click")
@@ -33,32 +33,44 @@ describe("test word guesses", () => {
   let inputBox
   let submitButton
 
-  beforeEach(() => {
-    ;[wrapper, inputBox, submitButton] = setup("party")
-  })
-
-  describe("correct guess", () => {
+  describe("non-empty guessedWords", () => {
     beforeEach(() => {
-      const mockEvent = { target: { value: "party" } }
-      inputBox.simulate("change", mockEvent)
-      submitButton.simulate("click")
+      ;[wrapper, inputBox, submitButton] = setup(["agile"], "party")
     })
 
-    test("Input component contains no children", () => {
-      const inputComponent = findByTestAttr(wrapper, "component-input")
-      expect(inputComponent.children().length).toBe(0)
-    })
-  })
+    describe("correct guess", () => {
+      beforeEach(() => {
+        const mockEvent = { target: { value: "party" } }
+        inputBox.simulate("change", mockEvent)
+        submitButton.simulate("click")
+      })
 
-  describe("incorrect guess", () => {
-    beforeEach(() => {
-      const mockEvent = { target: { value: "train" } }
-      inputBox.simulate("change", mockEvent)
-      submitButton.simulate("click")
+      test("Input component contains no children", () => {
+        const inputComponent = findByTestAttr(wrapper, "component-input")
+        expect(inputComponent.children().length).toBe(0)
+      })
+
+      test("GuessedWords table row count reflects updated guess", () => {
+        const guessedWordsTableRows = findByTestAttr(wrapper, "guessed-word")
+        expect(guessedWordsTableRows.length).toBe(2)
+      })
     })
 
-    test("Input box remains", () => {
-      expect(inputBox.exists()).toBe(true)
+    describe("incorrect guess", () => {
+      beforeEach(() => {
+        const mockEvent = { target: { value: "train" } }
+        inputBox.simulate("change", mockEvent)
+        submitButton.simulate("click")
+      })
+
+      test("Input box remains", () => {
+        expect(inputBox.exists()).toBe(true)
+      })
+
+      test("GuessedWords table row count reflects updated guess", () => {
+        const guessedWordsTableRows = findByTestAttr(wrapper, "guessed-word")
+        expect(guessedWordsTableRows.length).toBe(2)
+      })
     })
   })
 })
